dd1be8fe5100c7fcffd1ce6f317e248c62fa7c61
Kendrick <khe4r6@gmail.com>
2025-02-24 22:37:50 -0600

    Update Fetch Method

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
-import yfinance as yf
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import yfinance as yf
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])
 
-@app.route("/stock-symbol-valid", methods=["GET"])
-def get_stock_symbol_valid():
+
+@app.route("/stock-data", methods=["GET"])
+def get_stock_data():
     symbol = request.args.get("symbol")
+    expiration_date = request.args.get("expirationDate")
     if not symbol:
-        return jsonify({"error": "Missing parameters"}), 400
+        return jsonify({"error": "Missing Symbol"}), 400
     try:
+        print("Symbol: ", symbol)
         stock = yf.Ticker(symbol.lower())
-        if stock.info:
-            return jsonify(), 200
+        if not stock:
+            return jsonify({"error": "Invalid Symbol"}), 400
+        if expiration_date:
+            print("Expirate Date: ", expiration_date)
+            option_chain = stock.option_chain(expiration_date)
+        else:
+            print("No Expiration Date")
+            option_chain = stock.option_chain()
+        dates = list(stock.options)
+        stock_info = stock.info
+
+        # Calculate mark price
+        option_chain.calls["mark"] = (
+            option_chain.calls["bid"] + option_chain.calls["ask"]
+        ) / 2
+        option_chain.puts["mark"] = (
+            option_chain.puts["bid"] + option_chain.puts["ask"]
+        ) / 2
+
+        calls = option_chain.calls.fillna(value=0)
+        strikes = option_chain.calls[["strike"]].fillna(value=0)
+        puts = option_chain.puts.fillna(value=0)
+
+        response = {
+            "calls": calls.to_dict(orient="records"),
+            "dates": dates,
+            "puts": puts.to_dict(orient="records"),
+            "info": stock_info,
+            "strikes": strikes.to_dict(orient="records"),
+        }
+
+        return jsonify(response)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -36,78 +69,6 @@ def get_watch_list_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route("/stock-data-symbol", methods=["GET"])
-def get_stock_data_symbol():
-    symbol = request.args.get("symbol")
-    if not symbol:
-        return jsonify({"error": "Missing parameters"}), 400
-    try:
-        stock = yf.Ticker(symbol.lower())
-        option_chain = stock.option_chain()
-        expirations = list(stock.options)
-
-        stock_info = stock.info
-
-        # Calculate mark price
-        option_chain.calls["mark"] = (
-            option_chain.calls["bid"] + option_chain.calls["ask"]
-        ) / 2
-        option_chain.puts["mark"] = (
-            option_chain.puts["bid"] + option_chain.puts["ask"]
-        ) / 2
-
-        calls = option_chain.calls.fillna(value=0)
-        strikes = option_chain.calls[["strike"]].fillna(value=0)
-        puts = option_chain.puts.fillna(value=0)
-
-        response = {
-            "calls": calls.to_dict(orient="records"),
-            "expirations": expirations,
-            "strikes": strikes.to_dict(orient="records"),
-            "puts": puts.to_dict(orient="records"),
-            "info": stock_info,
-        }
-
-        return jsonify(response)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route("/stock-data-expiration", methods=["GET"])
-def get_stock_data_expiration():
-    symbol = request.args.get("symbol")
-    expiration = request.args.get("expiration")
-    if not symbol or not expiration:
-        return jsonify({"error": "Missing parameters"}), 400
-    try:
-        stock = yf.Ticker(symbol.lower())
-        option_chain = stock.option_chain(expiration)
-        dates = list(stock.options)
-
-        stock_info = stock.info
-
-        # Calculate mark price
-        option_chain.calls["mark"] = (
-            option_chain.calls["bid"] + option_chain.calls["ask"]
-        ) / 2
-        option_chain.puts["mark"] = (
-            option_chain.puts["bid"] + option_chain.puts["ask"]
-        ) / 2
-
-        calls = option_chain.calls.fillna(value=0)
-        strikes = option_chain.calls[["strike"]].fillna(value=0)
-        puts = option_chain.puts.fillna(value=0)
-
-        response = {
-            "calls": calls.to_dict(orient="records"),
-            "dates": dates,
-            "strikes": strikes.to_dict(orient="records"),
-            "puts": puts.to_dict(orient="records"),
-            "info": stock_info,
-        }
-
-        return jsonify(response)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
